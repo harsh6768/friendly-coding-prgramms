508c3d28ce55ea9c64b7eb9768531d84e9bb9d3c
Harsh Chaurasiya <harshchaurasiya6768@gmail.com>
2019-08-02 11:59:59 +0530

    Add files via upload

---
+++ python-programms/inheritance/mutiple.py
@@ -0,0 +1,35 @@
+class A:
+
+    def features1(self):
+        print('Features 1')
+    def features2(self):
+        print('Features 2')
+
+a1=A()
+#calling method of class A
+a1.features1()
+a1.features2()
+
+
+#Class B inheriting the Class A
+class B():
+    def features3(self):
+        print('Features 3')
+    def features4(self):
+        print('Features 4')
+    
+
+b1=B()
+b1.features3()
+#Multiple inheritance
+class C(A,B):
+    def features5(self):
+        print('Features 5')
+    def features6(self):
+        print('features 6')
+
+
+c1=C()
+c1.features2()
+c1.features5()
+c1.features6()
