a54114c7252f049ea498b1da8543338f0f985dea
Harsh Chaurasiya <harshchaurasiya6768@gmail.com>
2019-08-02 12:10:14 +0530

    Add files via upload

---
+++ js-programms/hoisting/hoisting.js
@@ -0,0 +1,14 @@
+
+console.log(b)
+var b=10
+var c 
+
+console.log(c)
+// function hello(){
+    
+// }
+
+let hello=function(){
+    c=10
+}
+hello()
---
+++ js-programms/hoisting/hoisting1.js
@@ -0,0 +1,34 @@
+
+console.log(`Value of h11:${h11}`)
+let x1
+function hello(){
+
+//    console.log(h1)
+    console.log(`Value of h:${h}`)
+    var h=0
+    console.log(`Value of h:${h}`)
+    h1=20
+    console.log(h1)
+
+    function hello1(){
+    
+        console.log(`Value of h2:${h2}`)
+        x1=0
+        var h2=0
+        console.log(`Value of h3:${h3}`)
+        var h3=21
+        //h1=30
+        console.log(`Value of h1:${h1}`)   
+    
+    }
+    //console.log(`Value of x:${x}`)
+    hello1()
+    console.log(`Value of x1:${x1}`)
+}
+
+var h11=0
+
+// var hel=
+hello()
+// console.log(h1)
+// console.log(h)  
